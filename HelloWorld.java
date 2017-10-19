36dff867ff80694f7c95581aba2d7ced7a40f50d
vivianewangs <32096621+vivianewangs@users.noreply.github.com>
2017-10-20 01:25:50 +0200

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hello.world;
+
+/**
+ *
+ * @author vivia
+ */
+public class HelloWorld {
+
+    /**第一个Java程序
+     * 他将打印字符串 Hello World
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello World");//打印 Hello World
+    }
+    
+}
